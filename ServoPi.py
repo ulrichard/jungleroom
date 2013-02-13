aab1928b4ed9e8d8bc059e70d360dab9a075d29c
richard Ulrich <richi@paraeasy.ch>
2013-02-13 20:45:32 +0000

    getting the new stuff to compile

--- ServoPi.py
+++ ServoPi.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python
 # see : http://docs.python.org/2/library/thread.html
 
-import time, thread, RPi.GPIO as GPIO
+import time, thread, threading, RPi.GPIO as GPIO
 
-class ServoPi:
+class ServoPi(threading.Thread):
 	def __init__(self, gpioPin, leftDuration = 0.001, rightDuration = 0.002, cycleDuration = 0.02):
 		self.terminate = False
 		self.active = False
@@ -12,31 +12,30 @@ class ServoPi:
 		self.rightDuration = rightDuration
 		self.cycleDuration = cycleDuration
 		self.currentDuration = leftDuration
-		self.threadId = thread.start_new_thread(threadFunc)
 		self.argLock  = thread.allocate_lock()
 	
-	def enable(val):
+	def enable(self, val):
 		with self.argLock:
 			self.active = val
 
-	def goTo(val): # fast transition -> val shall be a float between 0.0 and 1.0
+	def goTo(self, val): # fast transition -> val shall be a float between 0.0 and 1.0
 		interv = self.rightDuration - self.leftDuration
 		with self.argLock:
 			self.currentDuration = self.leftDuration + val * interv
 			self.active = True
 
-	def move(val, dur): # slow transition -> val shall be a float between 0.0 and 1.0
-		goTo(val)
+	def move(self, val, dur): # slow transition -> val shall be a float between 0.0 and 1.0
+		self.goTo(val)
 		
-	def threadFunc():
+	def run(self):
 		gpioPin = self.gpioPin
 		leftDuration  = self.leftDuration
 		rightDuration = self.rightDuration
 		while True:
 			with self.argLock:
-				if !self.terminate:
+				if not self.terminate:
 					break
-				if !self.active:
+				if not self.active:
 					time.sleep(100)
 					continue
 				
@@ -49,3 +48,15 @@ class ServoPi:
 				time.sleep(cycleDuration)
 				
 		
+# test code
+if __name__ == "__main__":
+	servo = ServoPi(4, 0.002, 0.001, 0.02)
+	servo.move(0, 1)
+	time.sleep(1)
+	servo.move(0.5, 1)
+	time.sleep(1)
+	servo.move(1, 1)
+	time.sleep(1)
+	servo.move(0, 1)
+	
+
